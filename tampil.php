93024fd79b4b829bdb020ba120de4139f8b27bca
Tegarluqman <109190581+Tegarluqman@users.noreply.github.com>
2023-02-26 17:30:04 -0800

    Add files via upload

--- tampil.php
+++ tampil.php
@@ -21,13 +21,30 @@
     <td>aksi</td>
 </tr>
 
+<?php
+include ("koneksi.php");
+$query=mysqli_query($db, "SELECT * FROM tb_jamaah INNER JOIN tb_paket
+ON tb_jamaah.id_jamaah = tb_paket.id_paket");
 
+while($row=mysqli_fetch_array($query)){
+    echo "<tr>";
+    echo "<td>".$row['nama_jamaah']."</td>";
+    echo "<td>".$row['alamat']."</td>";
+    echo "<td>".$row['jenis_kelamin']."</td>";
+    echo "<td>".$row['nama_paket']."</td>";
+    echo "<td>".$row['tujuan']."</td>";
+    echo "<td>".$row['tgl_keberangkatan']."</td>";
+    echo "<td>";
+    echo "<a href='edit.php?id=".$row['id_jamaah']."'>edit</a> |";
+    echo "<a href='hapus.php?id=".$row['id_jamaah']."'>hapus</a> ";
+    echo "</td>";
+    echo "</tr>";
+}
 
-
-
+?>
 </table>
 <br>
-
+<br>
 <a href="tambah.php"><input type="button" value="tambah">
 </body>
 </center>
